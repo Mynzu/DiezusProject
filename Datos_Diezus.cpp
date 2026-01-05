b563e4187a6204b3a393ad2ee12127ef5fec1fde
Kiry <kiraymalopez@gmail.com>
2026-01-04 20:45:09 -0400

    Agrego archivos de backend en C++

---
+++ Datos_Diezus.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+string nombre, contra;
+
+
+int Datos () {
+    cout<<"Ingrese su nombre" << endl;
+    cin >> nombre;
+    cout<<"Ingrese su contraseña" << endl;
+    cin>>contra;
+    return 0;
+}
--- Diezus2.cpp
+++ Diezus2.cpp
@@ -6,6 +6,27 @@ using namespace std;
 
 vector<string> frases_De_Salida = {
     "Quieres seguir hablando sobre esto? O prefieres escuchar mis chistes malos?",
+    "Te gustaría que exploremos otro detalle antes de despedirnos?",
+    "Antes de irme, ¿hay algo más que te gustaría saber?",
+    "¿Quieres que te cuente un dato curioso antes de terminar?",
+    "¿Hay algún otro tema que te interese discutir antes de que me vaya?",
+    "Hay algo más en lo que pueda ayudarte hoy?",
+    "¿Quieres que te recomiende un libro o una película antes de despedirnos?",
+    "¿Te gustaría recibir un consejo útil antes de que me despida?",
+    "¿Quieres que te comparta una cita inspiradora antes de irme?",
+    "¿Hay alguna otra pregunta que tengas en mente antes de que termine?",
+    "Hasta luego, ¡y que tengas un excelente día!"
+    "¿Quieres que te cuente un chiste rápido antes de despedirnos?"
+    "¿Hay algo más que te gustaría explorar antes de que me vaya?",
+    "¿Quieres que te dé una recomendación personalizada antes de irme?"
+    "¿Te gustaría que te comparta un dato interesante antes de despedirnos?",
+    "¿Hay algún otro tema que te gustaría discutir antes de que termine?",
+    "¿Quieres que te sugiera una actividad divertida antes de irme?"
+    "¿Hay algo más en lo que pueda asistirte antes de despedirnos?",
+    "¿Quieres que te cuente una historia breve antes de irme?",
+    "¿Hay algún otro tema que te gustaría discutir antes de que termine?",
+    "¿Quieres que te sugiera una actividad divertida antes de irme?",
+    "¿Hay algo más en lo que pueda asistirte antes de despedirnos?",
 };
 
 
